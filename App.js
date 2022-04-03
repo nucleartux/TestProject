bf0ec1252f11aa862556b04f82f68cf3450c3e2d
Adrov Igor <nucleartux@gmail.com>
2022-04-03 12:58:04 +0400

    first commit

---
+++ App.js
@@ -0,0 +1,83 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  Button,
+  TextInput,
+  FlatList,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {AvoidSoftInputView} from 'react-native-avoid-softinput';
+
+function HomeScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+      <Button onPress={() => navigation.navigate('List')} title="go to list" />
+    </View>
+  );
+}
+
+function ListScreen() {
+  return (
+    <View style={{flex: 1}}>
+      <AvoidSoftInputView style={{flex: 1}}>
+        <FlatList
+          data={[1, 2, 3, 4]}
+          renderItem={({item}) => (
+            <View style={{backgroundColor: 'blue'}}>
+              <Text>{item}</Text>
+            </View>
+          )}
+          inverted
+        />
+        <View>
+          <TextInput placeholder="Enter text" />
+        </View>
+      </AvoidSoftInputView>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+const App: () => Node = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          gestureEnabled: true,
+          fullScreenGestureEnabled: true,
+        }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="List" component={ListScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
